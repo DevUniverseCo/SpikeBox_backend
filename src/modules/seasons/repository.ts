13aeac535f15a488fad9bbb7ec891d8e6a0dd9a6
Michele Manzati <m.manzati@abletech.it>
2025-09-29 14:42:18 +0200

    Deleted all entities

--- src/modules/seasons/repository.ts
+++
@@ -1,7 +0,0 @@
-import { IBaseRepository } from "../../shared/common/base/repository";
-import { CreateSeason, Season } from "./domain";
-
-export interface ISeasonRepository
-  extends IBaseRepository<Season, CreateSeason> {
-  findAllWithTeamsFilteredClubId(id: string): Promise<any>;
-}
